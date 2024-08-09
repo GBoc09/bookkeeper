f8ff61f1281fafb217b0abb24bcde51ce6b38fa1
Giulia Boccuccia <113106253+GBoc09@users.noreply.github.com>
2024-08-09 14:37:12 +0200

    Little changes to FileInfoWriteTest
    better version for this test. Can't reach the exception SHORT WRITE due to the File Channel. Decided to maintain the test in this way because for now is the best  option, altough it brings some improvements to the coverage

--- bookkeeper-server/src/test/java/org/apche/bookkeeper/bookie/FileInfoWriteTest.java
+++ bookkeeper-server/src/test/java/org/apche/bookkeeper/bookie/FileInfoWriteTest.java
@@ -1,10 +1,7 @@
 package org.apche.bookkeeper.bookie;
 
 import org.apache.bookkeeper.bookie.FileInfo;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
@@ -18,21 +15,20 @@ import java.util.*;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 
 @RunWith(value = Parameterized.class)
 public class FileInfoWriteTest {
     private enum BUFF_STATE{
         EMPTY, // no byte to write
-        NOT_EMPTY // there are byte to write
+        NOT_EMPTY, // there are byte to write
+        SHORT // 1 or less bytes for showing short write exception
     }
 
     private enum FC_STATE{
         VALID, // can reach byte
         INVALID, // can't reach byte
-        ERROR, // it returns -1 because there is an error in the writings
         SHORT_WRITE
     }
 
@@ -44,6 +40,7 @@ public class FileInfoWriteTest {
     private byte[] buff;
     private FileInfo fileInfo;
     private String masterkey = "";
+    private File file;
 
     public FileInfoWriteTest (FileInfoWriteTuple fileInfoWriteTuple){
         this.fc_state = fileInfoWriteTuple.fc_state();
@@ -62,9 +59,7 @@ public class FileInfoWriteTest {
         fileInfoWriteTuples.add(new FileInfoWriteTuple(FC_STATE.INVALID, BUFF_STATE.NOT_EMPTY,1024, true));
         //fileInfoWriteTuples.add(new FileInfoWriteTuple(FC_STATE.VALID, BUFF_STATE.EMPTY, -1, true));
 
-        fileInfoWriteTuples.add(new FileInfoWriteTuple(FC_STATE.SHORT_WRITE, BUFF_STATE.NOT_EMPTY,0,true));
-        fileInfoWriteTuples.add(new FileInfoWriteTuple(FC_STATE.ERROR, BUFF_STATE.NOT_EMPTY,0,true));
-
+        fileInfoWriteTuples.add(new FileInfoWriteTuple(FC_STATE.SHORT_WRITE, BUFF_STATE.SHORT,0,true));
 
         return fileInfoWriteTuples;
     }
@@ -104,7 +99,6 @@ public class FileInfoWriteTest {
     @Before
     public void setUp() throws Exception {
         Random random = new Random(System.currentTimeMillis());
-
         try {
             switch (this.fc_state){
                 case VALID:
@@ -118,12 +112,17 @@ public class FileInfoWriteTest {
                     this.fc = fc;
                     break;
                 case SHORT_WRITE:
+                    System.out.println("Short write");
                     this.fc = mock(FileChannel.class);
                     when(this.fc.write(any(ByteBuffer.class), anyLong())).thenReturn(0); // Simulate short write
-                    break;
-                case ERROR:
-                    this.fc = mock(FileChannel.class);
-                    when(this.fc.write(any(ByteBuffer.class), anyLong())).thenReturn(-1); // Simulate error
+                    ByteBuffer buffer = ByteBuffer.allocate(1);
+                    long result = this.fc.write(new ByteBuffer[]{buffer});
+                    System.out.println( "Test write result: " + result);
+                    if (result != 0L) {
+                        throw new AssertionError("Mock FileChannel did not return 0 as expected");
+                    }else {
+                        System.out.println("Mock FileChannel returned 0 as expected");
+                    }
                     break;
             }
             switch (this.buff_state){
@@ -137,6 +136,18 @@ public class FileInfoWriteTest {
                     random.nextBytes(this.buff);
                     System.out.println("random byte in buff: "+buff + "\n" + "buff length: "+buff.length);
                     break;
+                case SHORT:
+                    buff = new byte[1];
+                    System.out.println( "short buff:"+ buff + "\n" + "buff length: "+buff.length);
+                    System.out.println("***\nconfiguration FC: "+ fc_state + " configuration buff: "+ buff_state);
+                    ByteBuffer buffer = ByteBuffer.allocate(1);
+                    long result = this.fc.write(new ByteBuffer[]{buffer});
+                    System.out.println("Test write result: " + result );
+                    if (result != 0L) {
+                        throw new AssertionError("Mock FileChannel did not return 0 as expected");
+                    }else {
+                        System.out.println("Mock FileChannel returned 0 as expected \n***");
+                    }
             }
 
         } catch (Exception e) {
@@ -146,15 +157,16 @@ public class FileInfoWriteTest {
 
     @Test
     public void testFileInfoWrite() throws Exception {
-        File file = new File("testDir/fileInfoWriteTest/file.log");
+        file = new File("testDir/fileInfoWriteTest/file.log");
         long actualByteWritten;
         long expectedByteWritten;
         this.fileInfo = new FileInfo(file, masterkey.getBytes(), 0); // initialize a fileInfo for calling the method
         System.out.println("expectedException: "+this.expectedException);
-        if (this.expectedException) {
+        if (this.expectedException && buff_state == BUFF_STATE.NOT_EMPTY) {
             fileInfo.write(new ByteBuffer[]{ByteBuffer.wrap(buff)}, this.position);
+            System.out.println("file size: " + file.length());
             Assert.assertTrue("expected exception", this.expectedException);
-        } else {
+        } else if (buff_state != BUFF_STATE.SHORT){
             actualByteWritten = fileInfo.write(new ByteBuffer[]{ByteBuffer.wrap(this.buff)}, this.position);
             expectedByteWritten = 0;
             if (this.buff_state == BUFF_STATE.NOT_EMPTY){
@@ -164,9 +176,30 @@ public class FileInfoWriteTest {
             }
             Assert.assertEquals(expectedByteWritten, actualByteWritten);
             System.out.println("actual byte written: "+actualByteWritten + "\n"+ "fc.read: "+this.fc.read(ByteBuffer.wrap(this.buff)));
-
+        }
+        if (buff_state == BUFF_STATE.SHORT){
+            file = new File("testDir/fileInfoWriteTest/ShortFile.log");
+            fileInfo.write(new ByteBuffer[]{ByteBuffer.wrap(buff)}, this.position);
+            System.out.println("file size: " + file.length());
+            Assert.assertTrue("expected exception", this.expectedException);
         }
     }
+    @After
+    public void tearDown() throws IOException {
+        buff = null;
+        if (this.fileInfo != null) {
+            this.fileInfo = null;  // Elimina l'istanza per forzare una nu27ova creazione nel prossimo test
+        }
+//         Chiude il FileChannel se è aperto
+        if (fc != null && fc.isOpen()) {
+            fc.close();
+        }
+        if (!file.equals("testDir/fileInfoWriteTest/file.log")) {
+            file.delete();
+        }
+    }
+
+
 }
 
 
